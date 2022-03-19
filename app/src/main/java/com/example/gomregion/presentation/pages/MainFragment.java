54a48fbd8cb188c5d132218a17c9ea651dbdc907
AlSergachev <sanyasergachev@yandex.by>
2022-03-19 12:50:39 +0300

    set autoHeight in MainFragment

--- app/src/main/java/com/example/gomregion/presentation/pages/MainFragment.java
+++ app/src/main/java/com/example/gomregion/presentation/pages/MainFragment.java
@@ -1,80 +1,75 @@
 package com.example.gomregion.presentation.pages;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.example.gomregion.R;
-import com.example.gomregion.databinding.FragmentMainBinding;
 import com.example.gomregion.domain.utilities.Links;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MainFragment extends Fragment {
-
 
     AppCompatButton btmMagazine;
     AppCompatButton btmGomelisp;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MainFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
+    CardView card;
+    ImageView img;
+    View mainView;
+    int widthDisplay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View inflaterView = inflater.inflate(R.layout.fragment_main, container, false);
-        btmMagazine = inflaterView.findViewById(R.id.btmMagazine);
-        btmGomelisp = inflaterView.findViewById(R.id.btmGomelisp);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        mainView = v;
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getWidthDisplay();
+        setImages();
+        btmMagazine = view.findViewById(R.id.btmMagazine);
+        btmGomelisp = view.findViewById(R.id.btmGomelisp);
         setListeners();
-        return inflaterView;
+    }
+
+    private void setImages() {
+        setImageToCard(R.drawable.img_header, R.id.card_0, 0.402F);
+        setImageToCard(R.drawable.img_2, R.id.card_1, 0.428F);
+        setImageToCard(R.drawable.img_3, R.id.card_2, 0.562F);
+        setImageToCard(R.drawable.img_4, R.id.card_3, 0.563F);
+    }
+
+    private void setImageToCard(int imgId, int cardId, float ratio) {
+        int heightImg = (int) (widthDisplay * ratio);
+        card = mainView.findViewById(cardId);
+        img = new ImageView(mainView.getContext());
+        img.setImageResource(imgId);
+        card.addView(img, widthDisplay, heightImg);
+    }
+
+    private void getWidthDisplay() {
+        WindowManager wm = (WindowManager) requireContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        widthDisplay = size.x;
     }
 
     private void setListeners() {
